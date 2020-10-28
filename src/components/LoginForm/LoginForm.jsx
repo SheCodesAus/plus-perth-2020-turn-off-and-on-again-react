53c5845261c4111d4b50822ed8bac11a6ccd97de
DebbyMurphy <debby.m.jones@gmail.com>
2020-10-28 14:03:16 +0800

    LoginForm component
    Placeholder CSS applied
    JSX code added

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,80 +3,82 @@ import { useHistory } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginForm() {
-    // variables 
-    const [credentials, setCredentials] = useState({
-        username: "",
-        password: "",
-    });
+  //variables
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const history = useHistory();
 
-    const history = useHistory();
+  //methods
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
+  };
 
-    // methods
-    const handleChange = (e) => {
-        const {id, value} = e.target;
-        setCredentials((prevCredentials) => ({
-            ...prevCredentials,
-            [id]: value,
-        }));
-    };
-
-    const postData = async () => {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}api-token-auth/`, {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(credentials),
-            })
-            return response.json();
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(credentials.username && credentials.password) {
-            postData().then((response) => {
-                window.localStorage.setItem("token", response.token);
-                history.push("/");
-            });
-        }
-    };
-
-    // template
-
-
-    return (
-        <form>
-            <div>
-                <h1>Login</h1>
-            </div>
-
-            <div>
-                <p id="form-p">Login to make a pledge or post a new case on Raise a Roof. Help someone on their journey to regain independence, food and shelter.</p>
-            </div>
-            
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input 
-                    type="text"
-                    id="username"
-                    placeholder="Enter username"
-                    onChange={handleChange}
-                />
-            </div>
-
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input 
-                    type="password"
-                    id="password"
-                    placeholder="Enter password"
-                    onChange={handleChange}
-                />
-            </div>
-            
-            <button type="submit" onClick={handleSubmit}>Login</button>
-        </form>
+  const postData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}api-token-auth/`,
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      }
     );
+
+    const data = await response.json();
+    return {
+      ok: response.ok,
+      ...data,
+    };
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (credentials.username && credentials.password) {
+      postData().then((response) => {
+        if (!response.ok) {
+          alert("Wrong details");
+          return;
+        }
+        window.localStorage.setItem("token", response.token);
+        history.push("/");
+        // console.log(response);
+      });
+    }
+  };
+
+  //template
+  return (
+    <form>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Enter username"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter password"
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Login
+      </button>
+    </form>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
