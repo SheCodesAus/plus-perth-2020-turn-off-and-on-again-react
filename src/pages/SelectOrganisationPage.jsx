fd8e615538ccda2955e4bd1aa14690e332141085
roxygia <62426542+roxygia@users.noreply.github.com>
2020-11-04 20:31:09 +0800

    Create SelectOrganisationPage.jsx
    placeholder only

---
+++ src/pages/SelectOrganisationPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SelectOrganisationPage() {
+
+    // template
+    return ( 
+        <div>
+            <div className="project-header">
+                <h1> Select Organisation </h1> 
+                <div id="react-search"></div>
+            </div>
+        </div>
+    );
+}
+
+export default SelectOrganisationPage;
