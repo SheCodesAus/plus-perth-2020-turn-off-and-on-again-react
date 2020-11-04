325b6827da9fc8be945ff4ab51a9a9a8414dfa1a
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-11-04 18:26:57 +0800

    Updated the App.js file and uncommented the OpportunityListPage

--- src/pages/OpportunityListPage.jsx
+++ src/pages/OpportunityListPage.jsx
@@ -1,9 +1,9 @@
-// import React from "react"
-// import PostOpportunityForm from "../components/PostOpportunityForm/PostOpportunityForm"
+import React from "react"
+import PostOpportunityForm from "../components/PostOpportunityForm/PostOpportunityForm"
 
-// function OpportunityListPage() {
-//   return <PostOpportunityForm />
-//   //   <h1>Opportunity List Page</h1>
-// }
+function OpportunityListPage() {
+  return <PostOpportunityForm />
+  //   <h1>Opportunity List Page</h1>
+}
 
-// export default OpportunityListPage
+export default OpportunityListPage
