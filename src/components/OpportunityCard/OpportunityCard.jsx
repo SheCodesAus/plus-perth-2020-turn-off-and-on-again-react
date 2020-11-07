bb6f3ccb0e17805b60c6f744e0297061154e8b02
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-11-07 14:22:06 +0800

    Updated the OpportunityCard. Created the functionality on the OpportunityDetailsPage. Created the EditOpportunityPage.

--- src/components/OpportunityCard/OpportunityCard.jsx
+++ src/components/OpportunityCard/OpportunityCard.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom"
 
 import "./OpportunityCard.css"
 
-
 function OpportunityCard({ opportunityData, ...props }) {
   return (
     <div className="listing-card">
-    <Link to={`/opportunities/${opportunityData.id}`}>
-        <img src={opportunityData.image} alt="opportunity" />
+      <Link to={`/opportunities/${opportunityData.id}`}>
+        <img src={opportunityData.image} />
         <h3>{opportunityData.title}</h3>
         <h3>{opportunityData.start_date}</h3>
         <h3>{opportunityData.organisation}</h3>
@@ -17,7 +16,7 @@ function OpportunityCard({ opportunityData, ...props }) {
         <h3>{opportunityData.description}</h3>
         <h3>{opportunityData.apply_by_date}</h3>
         <h3>{opportunityData.datecreated}</h3>
-        <p>Created by {opportunityData.owner}</p>
+        <p>Created by {opportunityData.user}</p>
       </Link>
     </div>
   )
