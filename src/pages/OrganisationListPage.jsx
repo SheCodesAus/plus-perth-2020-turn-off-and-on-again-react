1a836487a498c7595daa24e8e18250e89e5d627c
DebbyMurphy <debby.m.jones@gmail.com>
2020-11-11 18:38:06 +0800

    Updated OrganisationListPage code

--- src/pages/OrganisationListPage.jsx
+++ src/pages/OrganisationListPage.jsx
@@ -1,42 +1,47 @@
-import React, {useEffect,useState} from "react";
-import ReactLoading from "react-loading";
-import OrganisationCard from "../components/OrganisationCard/OrganisationCard";
+import React, { useEffect, useState } from "react"
+import ReactLoading from "react-loading"
+
+import OrganisationCard from "../components/OrganisationCard/OrganisationCard"
 
 function OrganisationListPage() {
-    // variables
-    const [organisationList, setOrganisationList] = useState({
-        loading: true
-    });
+  // variables
 
-    // methods: useEffect render when the app render, the bracket while have the condition for the useEffect to rerender when app change
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}organisation/`)
-            .then((results) => {
-                return results.json();
-            })
-            .then((data) => {
-                setOrganisationList(data);
-            });
-    }, [organisationList]);
+  const [organisationList, setOrganisationList] = useState({
+    loading: true,
+  })
 
-    if (organisationList.loading) {
-        return  <ReactLoading className = "bubbles" type = { "Bubbles" } color = { "#FE4A49" }/>
-    }
+  // methods: useEffect render when the app render, the bracket while have the condition for the useEffect to rerender when app change
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}organisations/`)
+      .then((results) => {
+        return results.json()
+      })
+      .then((data) => {
+        setOrganisationList(data)
+      })
+  }, [organisationList])
 
-    // template
-    return ( 
-        <div>
-            <div className="project-header">
-                <h1> This is the Organisation Page </h1> 
-                <div id="opportunity-list" > 
-                    {organisationList.map((organisationData, key) => {
-                    return <OrganisationCard key={key} organisationData={organisationData}/>;
-                    })
-                } 
-                </div> 
-            </div>
+  if (organisationList.loading) {
+    return (
+      <ReactLoading className="bubbles" type={"Bubbles"} color={"#FE4A49"} />
+    )
+  }
+
+  // template
+  return (
+    <div>
+      <div className="project-header">
+        <h1> This is the Organisation Page </h1>
+        <div id="organisation-list">
+          {organisationList.map((organisationData, key) => {
+            return (
+              <OrganisationCard key={key} organisationData={organisationData} />
+            )
+          })}
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
 export default OrganisationListPage
