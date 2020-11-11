7b34d30fa460dbd9951ab775f2fb39baf09fc3b2
DebbyMurphy <debby.m.jones@gmail.com>
2020-11-11 19:39:05 +0800

    Updates made to the Organisation List CSS

--- src/pages/SelectOrganisationPage.jsx
+++ src/pages/SelectOrganisationPage.jsx
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import { Link, useLocation } from "react-router-dom"
 
-import {
-    isAuthenticated,
-  } from "../components/Utilities/LocalStorage";
+import { isAuthenticated } from "../components/Utilities/LocalStorage"
 
 function SelectOrganisationPage() {
+  const location = useLocation()
+  const [isloggedin, setisloggedin] = useState(false)
 
-    const location = useLocation();
-    const [isloggedin, setisloggedin] = useState(false);
-    
-    useEffect(() => {
-        isAuthenticated() ? setisloggedin(true) : setisloggedin(false);
-    }, [location]);
+  useEffect(() => {
+    isAuthenticated() ? setisloggedin(true) : setisloggedin(false)
+  }, [location])
 
+  // template
+  return (
+    <div>
+      <div className="project-header">
+        <h1> Select Organisation </h1>
+        <div id="react-search"></div>
+      </div>
 
-    // template
-    return ( 
+      {isloggedin ? (
         <div>
-            <div className="project-header">
-                <h1> Select Organisation </h1> 
-                <div id="react-search"></div>
-            </div>
-
-            {isloggedin ? (
-            <div>
-              <Link id="link" to={`/organisations/register`}>
-                Register New Organisation
-              </Link>
-            </div>
-          ) : (
-            <div>
-            </div>
-          )}
+          <Link id="link" to={`/organisations/register`}>
+            Register New Organisation
+          </Link>
         </div>
-    );
+      ) : (
+        <div></div>
+      )}
+    </div>
+  )
 }
 
-export default SelectOrganisationPage;
+export default SelectOrganisationPage
