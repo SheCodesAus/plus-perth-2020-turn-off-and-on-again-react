1476f6a2e48e8c637ef268768125c3d9543d2a03
Gianazza <roxana.gianazza1@bhp.com>
2020-11-15 14:53:49 +0800

    updated CSS on opportunity form and about page
    - aligned css on opportunity page as per organisation
    - reduce size of "contact us form" on about page

--- src/pages/OpportunityDetailPage.jsx
+++ src/pages/OpportunityDetailPage.jsx
@@ -39,29 +39,21 @@ function OpportunityDetailPage() {
   }
 
   return (
-    <div>
-      <img src={opportunityData.image} alt={opportunityData.title}/>
-      <h3>{opportunityData.title}</h3>
-      <h3>{opportunityData.start_date}</h3>
+    <div className="detail-box">
+      <h2>{opportunityData.title}</h2>
       <h3>{opportunityData.organisation}</h3>
-      <h3>{opportunityData.website}</h3>
-      <h3>{opportunityData.description}</h3>
-      <h3>{opportunityData.apply_by_date}</h3>
-      <h3>{opportunityData.datecreated}</h3>
-      <h3>{opportunityData.is_open}</h3>
-      <p>Created by {opportunityData.user}</p>
-      <h4>
-        Created at:{" "}
-        {opportunityData.date_created
+      <a href={opportunityData.website}>{opportunityData.website}</a>
+      <p>{opportunityData.description}</p>
+      <h4>Apply by: {opportunityData.apply_by_date.substr(0, 10)}</h4>
+      <h4>Start date: {opportunityData.start_date.substr(0, 10)}</h4>
+      <img src={opportunityData.image} alt={opportunityData.title}/>
+      <p>Created by {opportunityData.owner}</p>
+      <p>Created on:{" "}{opportunityData.date_created
           ? opportunityData.date_created.substr(0, 10)
           : ""}
-      </h4>
-      <h4>{`Status: ${opportunityData.is_open}`}</h4>
+      </p>
 
-      <hr />
-      <button>
-        <Link to={`/opportunities/edit/${id}`}>Edit</Link>
-      </button>
+      <Link className="button-link" to={`/opportunities/edit/${id}`}>Edit</Link>
       <button type="delete" onClick={deleteData}>
         Delete
       </button>
